2abb89c56d9cb1064fe4b3cb9e2be9f8fdadc0c4
proxykingdev2 <117310322+proxykingdev2@users.noreply.github.com>
2022-11-09 00:32:38 +0330

    Update ip-forwarding.py

--- ip-forwarding.py
+++ ip-forwarding.py
@@ -13,7 +13,7 @@ foreign_vps_ip = input("Please enter your foriegn vps ip for forwrding traffic :
 print("FOREIGN-IP : {}".format(foreign_vps_ip))
 
 def ip_forwarding_linux():
-    commands = f"sudo sysctl net.ipv4.ip_forward=1 && sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination {iran_vps_ip} && sudo iptables -t nat -A PREROUTING -j DNAT --to-destination {foreign_vps_ip} && sudo iptables -t nat -A POSTROUTING -j MASQUERADE"
+    commands = f"sudo apt update -y && sudo sysctl net.ipv4.ip_forward=1 && sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination {iran_vps_ip} && sudo iptables -t nat -A PREROUTING -j DNAT --to-destination {foreign_vps_ip} && sudo iptables -t nat -A POSTROUTING -j MASQUERADE"
     os.system(commands)
     
     print(f"\nIp forwarding finished...\nPlease add this codes into /etc/rc.local for self boot auto start : #! bin/bash\nsudo sysctl net.ipv4.ip_forward=1\nsudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination {iran_vps_ip}\nsudo iptables -t nat -A PREROUTING -j DNAT --to-destination {foreign_vps_ip}\nsudo iptables -t nat -A POSTROUTING -j MASQUERADE\nexit0")
